15252eabce68c93ba0771d2d2ef30c39a27037fc
Alexander Golovanov <agolovanov@textpress.co>
2018-01-26 18:13:17 -0600

    Fix: remove flow type

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ module.exports = {
 
     "plugins": [
         "react",
-        "flowtype",
-        "flowtype-errors",
         "jest"
     ],
 
@@ -53,36 +51,9 @@ module.exports = {
         "react/jsx-uses-vars": 1,
         "react/jsx-uses-react": 1,
 
-        "flowtype-errors/show-errors": 2,
-
-        "flowtype/boolean-style": [ 2, "boolean" ],
-        "flowtype/define-flow-type": 1,
-        "flowtype/delimiter-dangle": [ 2, "never" ],
-        "flowtype/generic-spacing": [ 2, "never" ],
-        "flowtype/no-primitive-constructor-types": 2,
-        "flowtype/no-weak-types": 0,
-        "flowtype/object-type-delimiter": [ 2, "comma" ],
-        "flowtype/require-parameter-type": 0,
-        "flowtype/require-return-type": [ 0, "always", { "annotateUndefined": "never" } ],
-        "flowtype/require-valid-file-annotation": 0,
-        "flowtype/semi": [ 2, "always" ],
-        "flowtype/space-after-type-colon": [ 2, "always" ],
-        "flowtype/space-before-generic-bracket": [ 2, "never" ],
-        "flowtype/space-before-type-colon": [ 2, "never" ],
-        "flowtype/type-id-match": [ 2, "^([A-Z][a-z0-9]*)+$" ],
-        "flowtype/union-intersection-spacing": [ 2, "always" ],
-        "flowtype/use-flow-type": 1,
-        "flowtype/valid-syntax": 1,
-
         "jest/no-disabled-tests": 2,
         "jest/no-focused-tests": 2,
         "jest/no-identical-title": 2
 
-    },
-
-    "settings": {
-        "flowtype": {
-            "onlyFilesWithFlowAnnotation": true
-        }
     }
 };
